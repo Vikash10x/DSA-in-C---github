b9142033fc0a3137c37ede70b8177dbdd525faa9
Vikash Kumawat <vvikash0012@gmail.com>
2025-08-03 01:10:33 +0530

    Recursion que

--- DSA Codes/recursion-que.cpp
+++ DSA Codes/recursion-que.cpp
@@ -138,33 +138,76 @@ using namespace std;
 
 // --------------- LeetCode 121 que ---------
 
-void maxProfitFind(vector<int> &prices, int i, int &minPrice, int &maxProfit)
+// void maxProfitFind(vector<int> &prices, int i, int &minPrice, int &maxProfit)
+// {
+//     // bace case
+//     if (i == prices.size())
+//     {
+//         return;
+//     }
+
+//     // 1 case soln
+//     if (prices[i] < minPrice)
+//     {
+//         minPrice = prices[i];
+//     }
+//     int todayProfit = prices[i] - minPrice;
+//     if (todayProfit > maxProfit)
+//     {
+//         maxProfit = todayProfit;
+//     }
+//     // RR
+//     maxProfitFind(prices, i + 1, minPrice, maxProfit);
+// }
+// int main()
+// {
+//     vector<int> prices{4, 8, 1, 9, 3};
+//     int minPrice = INT_MAX;
+//     int maxProfit = INT_MIN;
+//     maxProfitFind(prices, 0, minPrice, maxProfit);
+//     cout << "Max profit is: " << maxProfit << endl;
+//     return 0;
+// }
+
+// --------- integer to English Words ---------
+
+vector<pair<int, string>> mp = {{1000000000, "Billion"}, {1000000, "Million"}, {1000, "Thousand"}, {100, "Hundred"}, {90, "Ninety"}, {80, "Eighty"}, {70, "Seventy"}, {60, "Sixty"}, {50, "Fifty"}, {40, "Forty"}, {30, "Thirty"}, {20, "Twenty"}, {19, "Nineteen"}, {18, "Eighteen"}, {17, "Seventeen"}, {16, "Sixteen"}, {15, "Fifteen"}, {14, "Fourteen"}, {13, "Thirteen"}, {12, "Twelve"}, {11, "Eleven"}, {10, "Ten"}, {9, "Nine"}, {8, "Eight"}, {7, "Seven"}, {6, "Six"}, {5, "Five"}, {4, "Four"}, {3, "Three"}, {2, "Two"}, {1, "One"}};
+
+string numberToWords(int num)
 {
-    // bace case
-    if (i == prices.size())
+    if (num == 0)
     {
-        return;
+        return "Zero";
     }
 
-    // 1 case soln
-    if (prices[i] < minPrice)
+    for (auto it : mp)
     {
-        minPrice = prices[i];
+        if (num >= it.first)
+        {
+            string a = "";
+            if (num >= 100)
+            {
+                a = numberToWords(num / it.first) + " ";
+            }
+
+            string b = it.second;
+
+            string c = "";
+            if (num % it.first != 0)
+            {
+                c = " " + numberToWords(num % it.first);
+            }
+            return a + b + c;
+        }
     }
-    int todayProfit = prices[i] - minPrice;
-    if (todayProfit > maxProfit)
-    {
-        maxProfit = todayProfit;
-    }
-    // RR
-    maxProfitFind(prices, i + 1, minPrice, maxProfit);
+    return "";
 }
 int main()
 {
-    vector<int> prices{4, 8, 1, 9, 3};
-    int minPrice = INT_MAX;
-    int maxProfit = INT_MIN;
-    maxProfitFind(prices, 0, minPrice, maxProfit);
-    cout << "Max profit is: " << maxProfit << endl;
+    int num;
+    cin >> num;
+    string ans = numberToWords(num);
+    cout << "Ans is: " << ans << endl;
+
     return 0;
 }
