4903ddea17032488cb4460797931cc3a948a30db
Vikash Kumawat <vvikash0012@gmail.com>
2025-08-04 23:02:46 +0530

    Recursion que

--- DSA Codes/recursion-que.cpp
+++ DSA Codes/recursion-que.cpp
@@ -171,43 +171,91 @@ using namespace std;
 
 // --------- integer to English Words ---------
 
-vector<pair<int, string>> mp = {{1000000000, "Billion"}, {1000000, "Million"}, {1000, "Thousand"}, {100, "Hundred"}, {90, "Ninety"}, {80, "Eighty"}, {70, "Seventy"}, {60, "Sixty"}, {50, "Fifty"}, {40, "Forty"}, {30, "Thirty"}, {20, "Twenty"}, {19, "Nineteen"}, {18, "Eighteen"}, {17, "Seventeen"}, {16, "Sixteen"}, {15, "Fifteen"}, {14, "Fourteen"}, {13, "Thirteen"}, {12, "Twelve"}, {11, "Eleven"}, {10, "Ten"}, {9, "Nine"}, {8, "Eight"}, {7, "Seven"}, {6, "Six"}, {5, "Five"}, {4, "Four"}, {3, "Three"}, {2, "Two"}, {1, "One"}};
+// vector<pair<int, string>> mp = {{1000000000, "Billion"}, {1000000, "Million"}, {1000, "Thousand"}, {100, "Hundred"}, {90, "Ninety"}, {80, "Eighty"}, {70, "Seventy"}, {60, "Sixty"}, {50, "Fifty"}, {40, "Forty"}, {30, "Thirty"}, {20, "Twenty"}, {19, "Nineteen"}, {18, "Eighteen"}, {17, "Seventeen"}, {16, "Sixteen"}, {15, "Fifteen"}, {14, "Fourteen"}, {13, "Thirteen"}, {12, "Twelve"}, {11, "Eleven"}, {10, "Ten"}, {9, "Nine"}, {8, "Eight"}, {7, "Seven"}, {6, "Six"}, {5, "Five"}, {4, "Four"}, {3, "Three"}, {2, "Two"}, {1, "One"}};
 
-string numberToWords(int num)
+// string numberToWords(int num)
+// {
+//     if (num == 0)
+//     {
+//         return "Zero";
+//     }
+
+//     for (auto it : mp)
+//     {
+//         if (num >= it.first)
+//         {
+//             string a = "";
+//             if (num >= 100)
+//             {
+//                 a = numberToWords(num / it.first) + " ";
+//             }
+
+//             string b = it.second;
+
+//             string c = "";
+//             if (num % it.first != 0)
+//             {
+//                 c = " " + numberToWords(num % it.first);
+//             }
+//             return a + b + c;
+//         }
+//     }
+//     return "";
+// }
+// int main()
+// {
+//     int num;
+//     cin >> num;
+//     string ans = numberToWords(num);
+//     cout << "Ans is: " << ans << endl;
+
+//     return 0;
+// }
+
+// ------------- Wildcard Matching -------------
+
+bool isMatchHelper(string &s, int si, string &p, int pi)
 {
-    if (num == 0)
+    if (si == s.size() && pi == p.size())
     {
-        return "Zero";
+        return true;
     }
 
-    for (auto it : mp)
+    if (si == s.size() && pi < p.size())
     {
-        if (num >= it.first)
+        while (pi < p.size())
         {
-            string a = "";
-            if (num >= 100)
-            {
-                a = numberToWords(num / it.first) + " ";
-            }
-
-            string b = it.second;
-
-            string c = "";
-            if (num % it.first != 0)
-            {
-                c = " " + numberToWords(num % it.first);
-            }
-            return a + b + c;
+            if (p[pi] != '*')
+                return false;
+            pi++;
         }
+        return true;
     }
-    return "";
+
+    if (s[si] == p[pi] || '?' == p[pi])
+    {
+        return isMatchHelper(s, si + 1, p, pi + 1);
+    }
+
+    if (p[pi] == '*')
+    {
+        bool caseA = isMatchHelper(s, si, p, pi + 1);
+
+        bool caseB = isMatchHelper(s, si + 1, p, pi);
+        return caseA || caseB;
+    }
+
+    return false;
 }
 int main()
 {
-    int num;
-    cin >> num;
-    string ans = numberToWords(num);
-    cout << "Ans is: " << ans << endl;
+    string s = "abcdef";
+    string p = "*****";
 
+    int si = 0;
+    int pi = 0;
+
+    bool ans = isMatchHelper(s, si, p, pi);
+    cout << "Ans is: " << ans << endl;
     return 0;
 }
