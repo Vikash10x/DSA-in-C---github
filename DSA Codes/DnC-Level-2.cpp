9afc838e12df728ea5220aa26c0bbd69ad2fe959
vvikash0012@gmail.com <vvikash0012@gmail.com>
2025-10-11 23:33:10 +0530

    DnC-Level-2

--- DSA Codes/DnC-Level-2.cpp
+++ DSA Codes/DnC-Level-2.cpp
@@ -2,111 +2,112 @@
 #include <vector>
 using namespace std;
 
-// int partition(int arr[], int s, int e)
-// {
-//     // step 1. Choose pivot element
-//     int pivotIndex = s;
-//     int pivotElement = arr[s];
-
-//     // step 2. find right positio for pivot Element and place it there
-//     int count = 0;
-//     for (int i = s + 1; i <= e; i++)
-//     {
-//         if (arr[i] <= pivotElement)
-//         {
-//             count++;
-//         }
-//     }
-//     // jab pivot ke bhar aau to mere pass pivot ki right position ka index ready hai
-//     int rightIndex = s + count;
-//     swap(arr[pivotIndex], arr[rightIndex]);
-//     pivotIndex = rightIndex;
-
-//     // step 3. left me chote and right me bade
-//     int i = s;
-//     int j = e;
-
-//     while (i < pivotIndex && j > pivotIndex)
-//     {
-//         while (arr[i] <= pivotIndex)
-//         {
-//             i++;
-//         }
-//         while (arr[j] > pivotIndex)
-//         {
-//             j--;
-//         }
-//     }
-
-//     if (i < pivotIndex && j > pivotIndex)
-//     {
-//         swap(arr[i], arr[j]);
-//     }
-
-//     return pivotIndex;
-// }
-
-// void quickSort(int arr[], int s, int e)
-// {
-//     // base case
-//     if (s >= e)
-//     {
-//         return;
-//     }
-
-//     // Partition
-//     int p = partition(arr, s, e);
-
-//     // sort of left array
-//     quickSort(arr, s, p - 1);
-
-//     // sort of right array
-//     quickSort(arr, p + 1, e);
-// }
-// int main()
-// {
-//     int arr[] = {8, 1, 2, 3, 20, 50, 35};
-//     int n = 7;
-
-//     int s = 0;
-//     int e = n - 1;
-//     quickSort(arr, s, e);
-
-//     for (int i = 0; i < n; i++)
-//     {
-//         cout << arr[i] << " ";
-//     }
-//     cout << endl;
-
-//     return 0;
-// }
-
-// ---------- BAck Tracking -----------
-
-void backTracking(string str, int i)
+int partition(int arr[], int s, int e)
 {
-    // Base case
-    if (i >= str.length())
+    // step 1. Choose pivot element
+    int pivotIndex = s;
+    int pivotElement = arr[s];
+
+    // step 2. find right positio for pivot Element and place it there
+    int count = 0;
+    for (int i = s + 1; i <= e; i++)
     {
-        cout << str << " ";
+        if (arr[i] <= pivotElement)
+        {
+            count++;
+        }
+    }
+    // jab pivot ke bhar aau to mere pass pivot ki right position ka index ready hai
+    int rightIndex = s + count;
+    swap(arr[pivotIndex], arr[rightIndex]);
+    pivotIndex = rightIndex;
+
+    // step 3. left me chote and right me bade
+    int i = s;
+    int j = e;
+
+    while (i < pivotIndex && j > pivotIndex)
+    {
+        while (arr[i] <= pivotIndex)
+        {
+            i++;
+        }
+        while (arr[j] > pivotIndex)
+        {
+            j--;
+        }
+    }
+
+    if (i < pivotIndex && j > pivotIndex)
+    {
+        swap(arr[i], arr[j]);
+    }
+
+    return pivotIndex;
+}
+
+void quickSort(int arr[], int s, int e)
+{
+    // base case
+    if (s >= e)
+    {
         return;
     }
 
-    // Recursive case
-    for (int j = i; j < str.length(); j++)
-    {
-        swap(str[i], str[j]);
-        backTracking(str, i + 1);
-        swap(str[i], str[j]); // backtrack
-    }
-}
+    // Partition
+    int p = partition(arr, s, e);
 
+    // sort of left array
+    quickSort(arr, s, p - 1);
+
+    // sort of right array
+    quickSort(arr, p + 1, e);
+}
 int main()
 {
-    string str = "pqrs";
-    int i = 0;
+    int arr[] = {8, 1, 2, 3, 20, 50, 35};
+    int n = 7;
+
+    int s = 0;
+    int e = n - 1;
     cout << endl;
-    backTracking(str, i);
+    quickSort(arr, s, e);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
     cout << endl;
+
     return 0;
 }
+
+// ---------- BAck Tracking -----------
+
+// void backTracking(string str, int i)
+// {
+//     // Base case
+//     if (i >= str.length())
+//     {
+//         cout << str << " ";
+//         return;
+//     }
+
+//     // Recursive case
+//     for (int j = i; j < str.length(); j++)
+//     {
+//         swap(str[i], str[j]);
+//         backTracking(str, i + 1);
+//         swap(str[i], str[j]); // backtrack
+//     }
+// }
+
+// int main()
+// {
+//     string str = "pqrs";
+//     int i = 0;
+//     cout << endl;
+//     backTracking(str, i);
+//     cout << endl;
+//     return 0;
+// }
