7f5f373c2d2e4ca67d9049003929efcbf32d6047
vvikash0012@gmail.com <vvikash0012@gmail.com>
2025-10-17 23:07:45 +0530

    DnC-Level-2

--- DSA Codes/DnC-Level-2.cpp
+++ DSA Codes/DnC-Level-2.cpp
@@ -2,85 +2,85 @@
 #include <vector>
 using namespace std;
 
-int partition(int arr[], int s, int e)
-{
-    // step 1. Choose pivot element
-    int pivotIndex = s;
-    int pivotElement = arr[s];
+// int partition(int arr[], int s, int e)
+// {
+//     // step 1. Choose pivot element
+//     int pivotIndex = s;
+//     int pivotElement = arr[s];
 
-    // step 2. find right positio for pivot Element and place it there
-    int count = 0;
-    for (int i = s + 1; i <= e; i++)
-    {
-        if (arr[i] <= pivotElement)
-        {
-            count++;
-        }
-    }
-    // jab pivot ke bhar aau to mere pass pivot ki right position ka index ready hai
-    int rightIndex = s + count;
-    swap(arr[pivotIndex], arr[rightIndex]);
-    pivotIndex = rightIndex;
+//     // step 2. find right positio for pivot Element and place it there
+//     int count = 0;
+//     for (int i = s + 1; i <= e; i++)
+//     {
+//         if (arr[i] <= pivotElement)
+//         {
+//             count++;
+//         }
+//     }
+//     // jab pivot ke bhar aau to mere pass pivot ki right position ka index ready hai
+//     int rightIndex = s + count;
+//     swap(arr[pivotIndex], arr[rightIndex]);
+//     pivotIndex = rightIndex;
 
-    // step 3. left me chote and right me bade
-    int i = s;
-    int j = e;
+//     // step 3. left me chote and right me bade
+//     int i = s;
+//     int j = e;
 
-    while (i < pivotIndex && j > pivotIndex)
-    {
-        while (arr[i] <= pivotIndex)
-        {
-            i++;
-        }
-        while (arr[j] > pivotIndex)
-        {
-            j--;
-        }
-    }
+//     while (i < pivotIndex && j > pivotIndex)
+//     {
+//         while (arr[i] <= pivotIndex)
+//         {
+//             i++;
+//         }
+//         while (arr[j] > pivotIndex)
+//         {
+//             j--;
+//         }
+//     }
 
-    if (i < pivotIndex && j > pivotIndex)
-    {
-        swap(arr[i], arr[j]);
-    }
+//     if (i < pivotIndex && j > pivotIndex)
+//     {
+//         swap(arr[i], arr[j]);
+//     }
 
-    return pivotIndex;
-}
+//     return pivotIndex;
+// }
 
-void quickSort(int arr[], int s, int e)
-{
-    // base case
-    if (s >= e)
-    {
-        return;
-    }
+// void quickSort(int arr[], int s, int e)
+// {
+//     // base case
+//     if (s >= e)
+//     {
+//         return;
+//     }
 
-    // Partition
-    int p = partition(arr, s, e);
+//     // Partition
+//     int p = partition(arr, s, e);
 
-    // sort of left array
-    quickSort(arr, s, p - 1);
+//     // sort of left array
+//     quickSort(arr, s, p - 1);
 
-    // sort of right array
-    quickSort(arr, p + 1, e);
-}
-int main()
-{
-    int arr[] = {8, 1, 2, 3, 20, 50, 35};
-    int n = 7;
+//     // sort of right array
+//     quickSort(arr, p + 1, e);
+// }
+// int main()
+// {
+//     int arr[] = {8, 1, 2, 3, 20, 50, 35};
+//     int n = 7;
 
-    int s = 0;
-    int e = n - 1;
-    cout << endl;
-    quickSort(arr, s, e);
+//     int s = 0;
+//     int e = n - 1;
+//     cout << endl;
+//     quickSort(arr, s, e);
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
 
-    return 0;
-}
+//     return 0;
+// }
 
 // ---------- BAck Tracking -----------
 
